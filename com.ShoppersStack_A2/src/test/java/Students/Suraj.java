0daf41687ea184c4cf50277262542785959981f1
LENOVO <LENOVO@Suraj>
2024-09-12 13:17:43 +0530

    Suraj

--- com.ShoppersStack_A2/src/test/java/Students/Suraj.java
+++ com.ShoppersStack_A2/src/test/java/Students/Suraj.java
@@ -1,5 +1,12 @@
 package Students;
 
 public class Suraj {
+	  
+public static void main(String[] args) {
+		
+		System.out.println("Hello Sir ");
+		System.out.println(" HAD YOU LUNCH ");
+	}
+
 
 }
